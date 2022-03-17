8539f559902361045df7333c16db8da55ba8f030
exi66 <58086435+exi66@users.noreply.github.com>
2022-03-17 23:52:23 +0300

    Update coupones.js

--- commands/bdo/coupones.js
+++ commands/bdo/coupones.js
@@ -1,5 +1,6 @@
 const request = require("request-promise-native");
 const { print_e } = require("../../functions.js");
+const fs = require("fs");
 
 module.exports = {
     name: "coupones",
@@ -9,42 +10,27 @@ module.exports = {
     usage: "<input>",
     run: (client, message, args, config) => {
         message.delete();
-		request({
-			method: "GET",
-			url: "https://orbit-games.com/",
-			headers: {
-				"User-Agent": "BDO-Umaru-bot https://github.com/exi66/BDO-Umaru-bot"
-			}
-		}).then(body => {
-			//console.log(body);
-			if (body !== '' && body != null) {	
-				let div = body.match(/<([^\s]+).*?id="text-15".*?>(.+?)<\/\1>/g);
-				let search = div[0].match(/\(?[a-zA-Z0-9]{4}\)?-?[a-zA-Z0-9]{4}?-?[a-zA-Z0-9]{4}-?[a-zA-Z0-9]{4}/gm);
-				if (!search) return print_e("[ERROR/coupones.js] div with coupones not found, need to edit regex");
-				let all_coupones_list = [];
-				for (let c of search) {
-					if (!all_coupones_list.includes(c.toUpperCase())) all_coupones_list.push(c.toUpperCase());
-				}
-				if (all_coupones_list.length > 0) {
-					let codes = "";
-					for (let c of all_coupones_list) {
-						codes += "`"+c+"`\n";
-					}
-					let em = {
+		try {
+			let coupons_list = JSON.parse(fs.readFileSync(config.coupons_folder));
+			if (coupons_list.length > 0) {
+				let codes = coupons_list.map(e => "`" + e + "`").join("\n");
+				try {
+					message.channel.send({
 						embed: {
 							color: 15105570,
 							title: "Купоны",
 							timestamp: new Date(),
 							description: codes
 						}
-					};	
-					try {
-						message.channel.send(em);
-					} catch (e) {
-						print_e("[ERROR/coupones.js]: "+e.message);
-					}						
-				} else return message.reply("нет доступных купонов(").then(m => m.delete({ timeout: 10000 }))	
-			}
-		}).catch(function(e){print_e("[ERROR/coupones.js]: "+e.message)});		
+					});
+				} catch (e) {
+					print_e("[ERROR/coupones.js]: "+e.message);
+				}						
+			} else return message.reply("нет доступных купонов(").then(m => m.delete({ timeout: 10000 }))	
+		}
+		catch (e) {
+			print_e("[ERROR/coupones.js]: "+e.message)
+			return message.reply("непредвиденная ошибка! Попробуйте позже.").then(m => m.delete({ timeout: 10000 }))
+		};		
     }
 }
