4577dc4b300e4e0ae77f5468a6c9dcedbab23fc5
exi66 <58086435+exi66@users.noreply.github.com>
2022-03-17 23:52:38 +0300

    Update queue.js

--- commands/bdo/queue.js
+++ commands/bdo/queue.js
@@ -1,5 +1,6 @@
 const request = require("request-promise-native");
 const { print_e } = require("../../functions.js");
+const fs = require("fs");
 
 module.exports = {
     name: "queue",
@@ -9,47 +10,34 @@ module.exports = {
     usage: "<input>",
     run: (client, message, args, config) => {
         message.delete();
-		request({
-			method: "GET",
-			url: "https://veliainn.com/api/market-queue/ru",
-			headers: {
-				"User-Agent": "BDO-Umaru-bot https://github.com/exi66/BDO-Umaru-bot"
-			}
-		}).then(body => {
-			if (body !== "" && body != null) {
-				let data = JSON.parse(body);
-				let items = data["items"];
-				//let lastupdate = data["lastUpdate"];
-				if (items.length > 0) {
-					let names = "", lvls = "", times = "";
-					for (let item of items) {
-						lvls += item[1]+"\n";
-						names += item[4]+"\n";
-						var date = new Date(item[3] * 1000);
-						var hours = date.getHours();
-						var minutes = "0" + date.getMinutes();
-						var seconds = "0" + date.getSeconds();
-						times += `${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}\n`;					
-					}
-					let em = {
+		try {
+			let queue_list = JSON.parse(fs.readFileSync(config.queue_folder));
+			let items = queue_list.items || [];
+			if (items.length > 0) {
+				let names = items.map(e => e[4]).join("\n"), 
+				lvls = items.map(e => e[1]).join("\n"), 
+				times = items.map(e => `${new Date(e[3] * 1000).getHours()}:${("0" + new Date(e[3]  * 1000).getMinutes()).substr(-2)}:${("0" + new Date(e[3] * 1000).getSeconds()).substr(-2)}`).join("\n");						
+				try {
+					message.channel.send({
 						embed: {
 							color: 15105570,
 							title: "Очередь аукциона",
-							timestamp: new Date(),
+							timestamp: new Date(queue_list.lastUpdate),
 							fields: [
 								{ name: "lvl", value: lvls, inline: true},
 								{ name: "Название", value: names, inline: true},
 								{ name: "Время", value: times, inline: true}
 							]
 						}
-					};							
-					try {
-						message.channel.send(em);
-					} catch (e) {
-						print_e("[ERROR/queue.js]: Cannot send message, "+e.message);
-					}
-				} else return message.reply("очередь аукциона пуста!").then(m => m.delete({ timeout: 10000 }))	
-			}
-		}).catch(function(e){print_e("[ERROR/queue.js]: "+e.message)});		
+					});
+				} catch (e) {
+					print_e("[ERROR/queue.js]: Cannot send message, "+e.message);
+				}
+			} else return message.reply("очередь аукциона пуста!").then(m => m.delete({ timeout: 10000 }))	
+		}
+		catch (e) {
+			print_e("[ERROR/queue.js]: "+e.message)
+			return message.reply("непредвиденная ошибка! Попробуйте позже.").then(m => m.delete({ timeout: 10000 }))
+		};		
     }
 }
